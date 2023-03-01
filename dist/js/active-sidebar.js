c07c7958e8403a1d853610accc1620500a6fdd9a
NicholassJP <99303569+NicholassJP@users.noreply.github.com>
2023-03-01 14:51:28 +0700

    sidebar fix

--- dist/js/active-sidebar.js
+++
@@ -1,6 +0,0 @@
-const activePage = window.location.pathname;
-const navLinks =  document.querySelectorAll('nav a').forEach(link => {
-  if(link.href === window.location.href) {
-    link.classList.add('active');
-  }
-});
